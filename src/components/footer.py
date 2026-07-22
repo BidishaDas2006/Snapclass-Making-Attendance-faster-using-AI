7fbaf66a41171e7b3d372d748399952c6ebc0f8a
Bidisha Das <biplobchandradas1974@gmail.com>
2026-07-22 18:48:41 +0530

    streamlit set up + home screen

---
+++ src/components/footer.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+def footer_home():
+    
+    st.markdown(f"""
+        <div style ="margin-top:2rem; display:flex; gap:6px;  align-items:center;; justify-content: center;">
+        <p style="font-weight:bold; color:white">Created with ❤️ by Bidisha</p>
+                
+                
+        </div>
+                
+
+            """, unsafe_allow_html = True)
