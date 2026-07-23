7c45e8794e61dba694596d0806953c29ce3766e0
Bidisha Das <biplobchandradas1974@gmail.com>
2026-07-23 20:38:40 +0530

    teacher screen auth set up

--- src/components/footer.py
+++ src/components/footer.py
@@ -11,3 +11,16 @@ def footer_home():
                 
 
             """, unsafe_allow_html = True)
+
+
+def footer_dashboard():
+    
+    st.markdown(f"""
+        <div style ="margin-top:2rem; display:flex; gap:6px;  align-items:center;; justify-content: center;">
+        <p style="font-weight:bold; color:black">Created with ❤️ by Bidisha</p>
+                
+                
+        </div>
+                
+
+            """, unsafe_allow_html = True)
